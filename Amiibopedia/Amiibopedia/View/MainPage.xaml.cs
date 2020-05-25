271f68869106bb20844d33d311f4740808be5457
Enrico86 <64355652+Enrico86@users.noreply.github.com>
2020-05-25 19:07:51 +0200

    Agregar archivos de proyecto.

---
+++ Amiibopedia/Amiibopedia/View/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Amiibopedia.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Amiibopedia
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPageViewModel ViewModel { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            ViewModel = new MainPageViewModel();
+            await ViewModel.LoadCharacters();
+            this.BindingContext = ViewModel;
+
+        }
+
+        private async void ViewCell_Appearing(object sender, EventArgs e)
+        {
+            var cell = sender as ViewCell;
+            var view = cell.View;
+            view.TranslationX = -100;
+            view.Opacity = 0;
+
+            await Task.WhenAny<bool>
+                (
+                    view.TranslateTo(0, 0, 500, Easing.Linear),
+                    view.FadeTo(1, 500, Easing.SinIn)
+                );
+            
+        }
+    }
+}
---
+++ Amiibopedia/Amiibopedia/ViewModel/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Amiibopedia.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy; set
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
